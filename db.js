fcbf2e80fc9483af0c7b626d161400da14efa18e
Alina <aasht@optrack.net>
2015-02-17 15:01:26 -0500

    fix for issue #3, #6 unit tests, routes, db logic

--- db.js
+++ db.js
@@ -1,241 +1,156 @@
 var env = require('./lib/environment');
 var mongoose = require('mongoose');
+var log = require('./lib/logger');
 
-//Variables for the schemas
+// Variables for the schemas
 var schema;
 var rankSchema;
-var rank;
 var studentSchema;
-var student;
 var classSchema;
-var course;
 var attendanceSchema;
-var attendance;
-var db;
 
-//Variable for connection condition
+
+// Variables for models
+var rank;
+var student;
+var course;
+var attendance;
+
+// Variable for connection condition
 var health = {
   connected: false,
   err: null
 };
 
-//Create connection
-db = mongoose.connection;
 
-//On disconnect
-db.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
+var connection = mongoose.connection;
+
+
+connection.on('disconnected', function () {
+    log.info('Mongoose default connection disconnected');
     health.connected = false;
 });
 
-db.on('disconnecting', function() {
-    rank.remove({}, function(err){
-      if(err){
-        return "Error";
-      }
-    });
-
-    student.remove({}, function(err){
-      if(err){
-        return "Error";
-      }
-    });
-});
-
-//On error
-db.on('error', function(error) {
+connection.on('error', function(error) {
     health.connected =  false;
     health.err = error;
-    console.log('Mongoose default connection error: '+error);
+    log.error({
+            message: "Mongoose default connection error:",
+            err: error
+    });
 });
-//db.on('error', console.error.bind(console, 'connection error:'));
 
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', function() {
-  db.close(function () {
-    rank.remove({}, function(err){
-        if(err){
-          return "Error";
-        }
-      });
-
-    student.remove({}, function(err){
-      if(err){
-        return "Error";
-      }
-    });
-    console.log('Mongoose default connection disconnected through app termination');
+  connection.close(function () {
+    log.info('Mongoose default connection disconnected through app termination');
     process.exit(0);
   });
 });
 
+connection.once('open', function (callback) {
+  health.connected = true;
+  log.info('connected');
+  schema = mongoose.Schema;
 
-db.once('open', function (callback) {
-    health.connected = true;
-    console.log("connected");
-     //Presumably connected
-     schema = mongoose.Schema;
+  /**
+  * Rank entity definition
+  */
+  rankSchema = new schema({
+    name: String,
+    sequence: Number,
+    color: String
+  });
 
-    //Rank entity
-    rankSchema = new schema({
-        name: String,
-        sequence: Number,
-        color: String
-    });
+  rank = mongoose.model('Rank', rankSchema);
 
-    rank = mongoose.model('Rank', rankSchema);
+  /**
+  * Student entity definition
+  */
+  studentSchema = new schema({
+    firstName: String,
+    lastName: String,
+    gender: String,
+    rankId : {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Rank'
+    },
+    healthInformation: String,
+    guardianInformation: String,
+    email: {
+      type: [String]
+    },
+    membershipStatus: Boolean,
+    membershipExpiry: Date,
+    phone: String,
+    birthDate: Date
+  });
 
-    //Student entity
-    studentSchema = new schema({
-        firstName :String,
-        lastName :String,
-        gender:String,
-        rankId : {type: mongoose.Schema.Types.ObjectId, ref: 'Rank'},
-        healthInformation:String,
-        guardianInformation:String,
-        email: {type: [String]},
-        membershipStatus: Boolean,
-        membershipExpiry:Date,
-        phone:String,
-        birthDate:Date
-    });
+  student = mongoose.model('Student', studentSchema);
 
-    student = mongoose.model('Student', studentSchema);
-
-    //Class entity
-    classSchema = new schema({
-        class_title: String,
-        start_date : Date,
-        end_date: Date,
-        day_of_week: Number,
-        start_time: Date,
-        end_time: Date,
-        classType: String,
-        RanksAllowed: {type: [mongoose.Schema.ObjectId], ref:'Rank'}
-    });
-
-    course= mongoose.model('Class', classSchema);
-
-    //Attendance entity
-    attendanceSchema = new schema({
-        student_id: {type: mongoose.Schema.Types.ObjectId, ref:'Student'},
-        classDate: Date,
-        classTime: Date,
-        classID: {type: mongoose.Schema.Types.ObjectId, ref: 'Class'}
-    });
-
-    attendance= mongoose.model('Attendance', attendanceSchema);
-
-    var blackBelt = new rank({
-      "name": "Black Belt",
-      "sequence": 2,
-      "color": "Black"
-    });
-
-    blackBelt.save(function (err, blackBelt){
-      if (err) return console.error(err);
-    });
-
-    var whiteBelt = new rank({
-      "name": "White Belt",
-      "sequence": 1,
-      "color": "White"
-    });
-
-    whiteBelt.save(function (err, whiteBelt){
-      if (err) return console.error(err);
-    });
-
-    var newStud = new student({
-          "firstName": "Damon",
-          "lastName": "Salvatore",
-          "gender": "M",
-           "rankId": "54da74e15fac9fec3c848fab",
-           "healthInformation":"healthy",
-          "guardianInformation": "stephan",
-           "email": "damon@salvatore.com",
-          "membershipStatus": true,
-          "membershipExpiry": "2009-04-12T20:44:55",
-          "phone": "444-333-3333",
-          "birthDate": "2009-04-12T20:44:55"
-        });
-
-    newStud.save(function (err, newStud) {
-      if (err) {
-        return console.log(err);
-      };
-    });
-
-    var newStud2 = new student({
-          "firstName": "Billy",
-          "lastName": "Madison",
-          "gender": "F",
-           "rankId": "54da74e15fac9fec3c848fab",
-           "healthInformation":"dead",
-          "guardianInformation": "bob",
-           "email": "example@myface.com",
-          "membershipStatus": false,
-          "membershipExpiry": "2009-04-12T20:44:55",
-          "phone": "454-323-3312",
-          "birthDate": "2004-04-12T20:44:55"
-        });
-
-    newStud2.save(function (err, newStud2) {
-      if (err) {
-        return console.log(err);
-      };
-    });
-
-    // FOR MOCHA TESTING:
-    // If we're running as a child process, let our parent know we're ready.
-    if (process.send) {
-      try {
-        process.send("serverStarted");
-      } catch ( e ) {
-        // exit the worker if master is gone
-        process.exit(1);
-      }
+  /**
+  * Class entity definition
+  */
+  classSchema = new schema({
+    class_title: String,
+    start_date : Date,
+    end_date: Date,
+    day_of_week: Number,
+    start_time: Date,
+    end_time: Date,
+    classType: String,
+    RanksAllowed: {
+      type: [mongoose.Schema.ObjectId], 
+      ref:'Rank'
     }
-});//Populate with a couple of entities
+  });
+
+  course= mongoose.model('Class', classSchema);
+
+  /**
+  * Attendance entity definition
+  */
+  attendanceSchema = new schema({
+    student_id: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref:'Student'
+    },
+    classDate: Date,
+    classTime: Date,
+    classID: {
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Class'
+    }
+  });
+
+  attendance= mongoose.model('Attendance', attendanceSchema);
+  // FOR MOCHA TESTING:
+  // If we're running as a child process, let our parent know we're ready.
+  if (process.send) {
+    try {
+      process.send("serverStarted");
+    } catch ( e ) {
+      // Exit the worker if master is gone
+      process.exit(1);
+    }
+  }
+});
 
 mongoose.connect(env.get("DBHOST"));
 
 module.exports = {
   "mongoose": mongoose,
-  "studentSchema": studentSchema,
   "student": mongoose.models.Student,
   "rank": mongoose.models.Rank,
   "course": mongoose.models.Course,
   "attendance": mongoose.models.Attendance,
-  "healthCheck": function (req, res, next){
-      if (health.connected)
-      {
-        next();
-      }
-      else{
-         next( new Error( "MongoDB: No connection found!" ) );
-      }
+  "healthCheck": function (req, res, next) {
+    if (!health.connected) {
+      return next( new Error( "MongoDB: No connection found!" ) );
+    }
+    else {
+       next();
+    }
   }
 };
-//methods that I need
-/*Student
-createStudent() done
-updateStudent() done
-getAllStudents() done
-getStudentById(id) done
-getStudentByName(name) done
-getStudentsByRank(rank) done
-getStudentsByGender(gender) done
-getStudentsByStatus(status) done
-addClass()
-updateClass()
-getAllClasses()
-getClassById(id)
-getClassByName(name)
-getClassesByDate(date)
-createAttendanceRecord()
-getAttendingStudentsByName(name)
-getAttendingStudentsByRank(rank)
-getAttendingStudentsByGender(gender)
-getAttendingStudentsByDate(date)
-getAttendingStudentsByClass(class)*/
+
--- routes/index.js
+++ routes/index.js
@@ -1,134 +1,150 @@
+var router;
+var env;
+var log;
+var db;
+
 module.exports = function() {
-  var router = require('express').Router();
-  var env = require('../lib/environment');
-  var log = require('../lib/logger');
-  var db = require('../db');
+  router = require('express').Router();
+  env = require('../lib/environment');
+  log = require('../lib/logger');
+  db = require('../db');
 
   router.route('/students')
-    .get(function(req, res, next) {
-      /* GET '/students' accepts:
+    /* GET '/students' accepts:
             - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
 
           GET '/students' returns:
             - A status code of success/failure
             - The entire student list via an array of JSON objects bearing every student stored in DB
-      */
+    */
+    .get(function(req, res, next) {
 
-      if(res.status == 401) {
-        res.status(401).send('Invalid login credentials');
-      }
+      // TODO: Check for authorization: https://github.com/sedge/opendojo/issues/22
 
-      if(res.status == 500) {
-        log.error({
+      db.mongoose.models.Student.find({}, function(err, students) {
+        if(err) {
+          res.status(500).send('Database Error!');
+          log.error({
+            err: err,
+            req: req,
+            res: res
+          });
+          return;
+        }
+
+        res.status(200).json(students);
+        log.info({
           req: req,
           res: res
         });
-      }
-
-      else {
-        db.mongoose.models.Student.find({}, function(err, students){
-          if(err) {
-            res.send('Database Error!', err);
-            return err;
-          }
-
-            res.status(200).json(students);
-        });
-      }
+      });
     })
-    .post(function(req, res, next) {
-      /* POST '/students' accepts:
+
+    /* POST '/students' accepts:
             - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
             - (1) new object student with properties in accordance with data model design in JSON format
 
           POST '/students' returns:
             - A status code of success/failure
             - If successful, the new student object data passed in by the request
-      */
+    */
+    .post(function(req, res, next) {
+      var validData = true;
+      var status=false;
+      var expiry="";
 
-      if(!req.body || req.body == {}) {
-        res.status(400).send('Request body has no valid data!');
-
-        return;
-      }
-
-      if(res.status == 500) {
-        log.error({
+      if(!req.body) {
+        log.warn({
           req: req,
           res: res
         });
+        res.status(400).send('Request body has no valid data!');
+        return;
+      }
+      
+      if ((!req.body.firstName) || (!req.body.lastName) || (!req.body.gender) || (!req.body.email) || (!req.body.phone) || (!req.body.birthDate)) {
+        validData = false;
       }
 
-      var student = db.mongoose.models.Student;
+      if (req.body.membershipStatus) {
+        status = req.body.membershipStatus;
+      }
 
-      var newStud = new student({
-        "firstName": req.body.firstName,
-        "lastName": req.body.lastName,
-        "gender": req.body.gender,
-        "rankId": req.body.rankId,
-        "healthInformation": req.body.healthInformation,
-        "guardianInformation": req.body.guardianInformation,
-        "email": req.body.email, //this is an array and should be passed in as such on the client request
-        "membershipStatus": req.body.membershipStatus,
-        "membershipExpiry": req.body.membershipExpiry,
-        "phone": req.body.phone,
-        "birthDate": req.body.birthDate
-      });
+      if (req.body.membershipExpiry ) {
+        expiry = req.body.membershipExpiry;
+      }
 
-      newStud.save(function (err, newStud) {
-        if (err) {
-          return next(err);
-        }
+      if (validData) {
+        var student = db.mongoose.models.Student;
 
-        res.status(201).json(newStud);
-      });
+        var newStud = new student({
+          "firstName": req.body.firstName,
+          "lastName": req.body.lastName,
+          "gender": req.body.gender,
+          "rankId": req.body.rankId, 
+          "healthInformation": req.body.healthInformation, 
+          "guardianInformation": req.body.guardianInformation, 
+          "email": req.body.email, 
+          "membershipStatus": req.body.membershipStatus,
+          "membershipExpiry": req.body.membershipExpiry,
+          "phone": req.body.phone,
+          "birthDate": req.body.birthDate
+        });
+        
+        newStud.save(function (err, newStud) {
+          if (err) {
+            log.error({
+              err: err,
+              req: req,
+              res: res
+            });
+            return next(err);
+          }
+          res.status(201).json(newStud);
+        });
+      }
+      else {
+        log.warn({
+          req: req,
+          res: res
+        });
+        res.status(400).send('Invalid request body!');
+      }
   });
 
   router.route('/student/:id')
-    .get(function(req, res, next) {
-      /* GET '/student/:id' accepts:
+    /* GET '/student/:id' accepts:
             - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
             - The ID of the object being requested (in JSON format ie. {"student_id": "6"})
 
           GET '/student/:id' returns:
             - A status code of success/failure
             - The student object bearing the given ID and all of its associated properties and values
-      */
+    */
+    .get(function(req, res, next) {
+ 
+      var id = req.params.id;
 
-      if(res.status == 400) {
-        res.status(400).send('Invalid request body!');
-      }
+      // TODO: Check for authorization: https://github.com/sedge/opendojo/issues/22
 
-      if(res.status == 401) {
-        res.status(401).send('Invalid login credentials');
-      }
-
-      if(res.status == 500) {
+      db.mongoose.models.Student.findById(id, function(err, student) {
+        if (err) {
         log.error({
+            err: err,
+            req: req,
+            res: res
+          });
+          return res.status(400).send('ID not found!');
+        }
+        log.info({
           req: req,
           res: res
         });
-      }
-
-      else {
-        var id = req.body.id || req.params.id;
-
-        db.mongoose.models.Student.findById(id, function(err, student) {
-          console.log('are we in findById');
-          if (err) {
-            return next(err);
-          }
-
-          if (typeof(student) === undefined) {
-            return "Student ID invalid!";
-          }
-
-          res.status(200).json(student);
-        });
-      }
+        res.status(200).json(student);
+      });
     })
-    .put(function(req, res, next) {
-      /* PUT '/student/:id' accepts:
+
+    /* PUT '/student/:id' accepts:
             - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
             - The ID of the corresponding object to be altered (in JSON format)
             - The property being altered and its new value in the request body
@@ -136,196 +152,107 @@ module.exports = function() {
           PUT '/student/:id' returns:
             - A status code of success/failure
             - If successful, the altered student data object passed in by the request
-      */
+    */
+    .put(function(req, res, next) {
+      
+      
+      var validData = true;
+      var studentProperties = [
+      "_id","firstName","lastName","gender",
+      "rankId","healthInformation","guardianInformation", 
+      "email", "membershipStatus","membershipExpiry",
+      "phone","birthDate" 
+      ]; 
+     
+      if(!req.body) {
+        res.status(400).send('Request body has no valid data!');
+        return;
+      }
+      
+      var id = req.params.id;
 
-      if(res.status == 400) {
-        res.status(400).send('Invalid request body!');
+      Object.keys(req.body).forEach(function(key) { 
+       if(studentProperties.indexOf(key)==-1) {
+        validData = false;
+       }
+      });
+
+      if (!validData) {
+        res.status(400).send('Request body has invalid data!');
+        return;
       }
 
-      if(res.status == 401) {
-        res.status(401).send('Invalid login credentials');
-      }
-
-      if(res.status == 500) {
-        log.error({
+      db.mongoose.models.Student.findOneAndUpdate({"_id": id}, req.body, {new: true}, function(err, student) {
+        if (err && err.name === "CastError") {      
+          log.warn({
+            req: req,
+            res: res
+          });
+           return res.status(400).send('ID not found!');
+        }
+        log.info({
           req: req,
           res: res
         });
-      }
-
-      else {
-        var id = req.body.id || req.params.id;
-
-        db.mongoose.models.Student.findOneAndUpdate({"_id": id}, req.body, {new: true}, function(err, student) {
-          if(err) {
-              return next(err);
-          }
-
-          res.status(200).json(student);
-        });
-      }
+        res.status(200).json(student);
+      });
     })
-    .delete(function(req, res, next) {
-      /* DELETE '/student/:id' accepts:
+
+    /* DELETE '/student/:id' accepts:
             - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
             - The student ID of the corresponding object to be deleted
 
           DELETE '/student/:id' returns:
             - A status code of success/failure
-      */
+    */
+    .delete(function(req, res, next) {
 
-      if(res.status == 400) {
-        res.status(400).send('Invalid request body!');
-      }
-
-      if(res.status == 401) {
-        res.status(401).send('Invalid login credentials');
-      }
-
-      if(res.status == 500) {
-        log.error({
-          req: req,
-          res: res
-        });
-      }
-
-      else {
-        var id = req.body.id || req.params.id;
-
-        db.mongoose.models.Student.remove({_id: id}, function(err){
-            if(err){
-              return next(err);
-            }
-            res.status(204).send('Success');
-        });
-      }
+      // TODO: Check for authorization: https://github.com/sedge/opendojo/issues/22
+       
+      var id = req.params.id;
+      db.mongoose.models.Student.remove({_id: id}, function(err) {
+        if(err) {
+          log.error({
+            req: req,
+            res: res,
+            err: err
+          });
+          res.status(204).send('Operation completed');
+        }
+      log.info({
+        req: req,
+        res: res
+      });
+      res.status(204).send('Operation completed');
+    });
   });
 
-  router.get('/rank', function(req, res, next) {
-    /* GET '/rank' accepts:
+  /* GET '/rank' accepts:
           - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
 
         GET '/rank' returns:
           - A status code of success/failure
           - All rank objects in the database
-    */
+  */
+  router.get('/rank', function(req, res, next) {
 
-    if(res.status == 400) {
-      res.status(400).send('Invalid request body!');
-    }
-
-    if(res.status == 401) {
-      res.status(401).send('Invalid login credentials');
-    }
-
-    if(res.status == 500) {
-      log.error({
-        req: req,
-        res: res
-      });
-    }
-
-    else {
-      db.mongoose.models.Rank.find({}, function(err, ranks){
+      db.mongoose.models.Rank.find({}, function(err, ranks) {
         if(err) {
-          res.send('Database Error!', err);
-          return err;
+          res.status(500).send('Database Error!');
+          log.error({
+            err: err,
+            req: req,
+            res: res
+          });
+          return;
         }
 
         res.status(200).json(ranks);
+        log.info({
+          req: req,
+          res: res
+        });
       });
-    }
   });
-
-  router.get('/students/rank/:colour', function(req, res, next) {
-      /* GET '/students/rank/:colour' accepts:
-            - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
-            - The rank of the student objects specified by the route value (ie. "1" || "blue")
-
-          GET '/students/rank/:colour' returns:
-            - A status code of success/failure
-            - All student objects bearing the given rank values
-      */
-
-      if(res.status == 400) {
-        res.status(400).send('Invalid request body!');
-      }
-
-      if(res.status == 401) {
-        res.status(401).send('Invalid login credentials');
-      }
-
-      if(res.status == 500) {
-        log.error({
-          req: req,
-          res: res
-        });
-      }
-
-      else {
-        var colour = req.body.colour || req.params.colour;
-
-        db.mongoose.models.Rank.find({color: colour}, function(err, rank){
-          if (err) {
-            return next(err);
-          }
-
-          db.mongoose.models.Student.find({rankId: rank._id}, function(err, students) {
-            if (err) {
-              return next(err);
-            }
-
-            if (typeof(students) === undefined) {
-              return "Rank reference invalid!";
-            }
-
-            res.status(200).json(students);
-          });
-        });
-      }
-  });
-
-  router.get('/students/member/:status', function(req, res, next) {
-      /* GET '/students/member/:status' accepts:
-            - Auth/Session tokens required from successful user sign-in to check for appropriate role/claim
-            - The status value of the objects being passed in the route (ie. "true" || "false")
-
-          GET '/students/member/:status' returns:
-            - A status code of success/failure
-            - All student objects bearing the specified status
-      */
-
-      if(res.status == 400) {
-        res.status(400).send('Invalid request body!');
-      }
-
-      if(res.status == 401) {
-        res.status(401).send('Invalid login credentials');
-      }
-
-      if(res.status == 500) {
-        log.error({
-          req: req,
-          res: res
-        });
-      }
-
-      else {
-        var status = req.body.status || req.params.status;
-
-        db.mongoose.models.Student.find({membershipStatus: status}, function(err, students) {
-          if (err) {
-            return next(err);
-          }
-
-          if (typeof(students) === undefined) {
-            return "Membership status invalid!";
-          }
-
-          res.status(200).json(students);
-        });
-      }
-  });
-
   return router;
 }();
