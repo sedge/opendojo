0b4b23239cd8f0973ea43d367ca51c912f3bed90
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-02-17 15:30:32 -0500

    Fixed our mocha/grunt integration

--- gruntfile.js
+++ gruntfile.js
@@ -5,20 +5,27 @@ module.exports = function(grunt){
       all: ['Gruntfile.js', 'app.js', 'public/**/*.js', 'server/**/*.js', 'routes/**/*.js']
     },
     //test suite
-    simplemocha: {
-      options: {
-        reporter: 'spec'
-      },
-      all: {
-        src: 'tests/index.js'
+    exec: {
+      run_mocha: {
+        command: 'mocha --timeout 1000 --recursive --reporter spec tests',
+        options: {
+          // Use the env from the process, but alter a few things for logging
+          env: (function(env) {
+            env.LOG_LEVEL = 'fatal';
+            env.NODE_ENV = 'development';
+            return env;
+          }(process.env))
+        },
+        stdout: true,
+        stderr: true
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-exec');
 
   // Default is the same as test, for travis-ci
   grunt.registerTask('default', 'test');
-  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['jshint', 'exec:run_mocha']);
 };
