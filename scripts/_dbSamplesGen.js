536dd1ef7cf1a94901dada24a5bb10a61507a043
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-03-30 11:13:50 -0400

    Fixed a script

--- scripts/_dbSamplesGen.js
+++ scripts/_dbSamplesGen.js
@@ -1,5 +1,5 @@
 var db = require("../server/lib/db");
-var models = require("../server/models");
+var models = require("../server/models")(db);
 
 function _generateSampleData() {
   var blackBelt = new models.Rank({
@@ -14,34 +14,6 @@ function _generateSampleData() {
     "color": "White"
   });
 
-  var newStud = new models.Student({
-    "firstName": "Damon",
-    "lastName": "Salvatore",
-    "gender": "M",
-    "rankId": "54da74e15fac9fec3c848fab",
-    "healthInformation":"healthy",
-    "guardianInformation": "stephan",
-    "email": "damon@salvatore.com",
-    "membershipStatus": true,
-    "membershipExpiry": "2009-04-12T20:44:55",
-    "phone": "444-333-3333",
-    "birthDate": "2009-04-12T20:44:55"
-  });
-
-  var newStud2 = new models.Student({
-    "firstName": "Billy",
-    "lastName": "Madison",
-    "gender": "F",
-    "rankId": "54da74e15fac9fec3c848fab",
-    "healthInformation": "dead",
-    "guardianInformation": "bob",
-    "email": "example@myface.com",
-    "membershipStatus": false,
-    "membershipExpiry": "2009-04-12T20:44:55",
-    "phone": "454-323-3312",
-    "birthDate": "2004-04-12T20:44:55"
-  });
-
   blackBelt.save(function (err, blackBelt){
     if(err){
       console.log("Error generating! ", err);
@@ -53,8 +25,36 @@ function _generateSampleData() {
         console.log("Error generating! ", err);
         return process.exit(1);
       }
-
       console.log('All ranks happily generated!');
+
+      var newStud = new models.Student({
+        "firstName": "Damon",
+        "lastName": "Salvatore",
+        "gender": "M",
+        "rankId": blackBelt._id,
+        "healthInformation":"healthy",
+        "guardianInformation": "stephan",
+        "email": "damon@salvatore.com",
+        "membershipStatus": true,
+        "membershipExpiry": "2009-04-12T20:44:55",
+        "phone": "444-333-3333",
+        "birthDate": "2009-04-12T20:44:55"
+      });
+
+      var newStud2 = new models.Student({
+        "firstName": "Billy",
+        "lastName": "Madison",
+        "gender": "F",
+        "rankId": whiteBelt._id,
+        "healthInformation": "dead",
+        "guardianInformation": "bob",
+        "email": "example@myface.com",
+        "membershipStatus": false,
+        "membershipExpiry": "2009-04-12T20:44:55",
+        "phone": "454-323-3312",
+        "birthDate": "2004-04-12T20:44:55"
+      });
+
       newStud.save(function (err, newStud) {
         if(err){
           console.log("Error generating! ", err);
