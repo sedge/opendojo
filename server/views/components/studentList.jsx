2489beedf5186631ebbbf3281ac584649accad0a
chris_choi <ssuduruk1@hotmail.com>
2015-03-31 18:08:46 -0400

    search working

--- server/views/components/studentList.jsx
+++ server/views/components/studentList.jsx
@@ -1,5 +1,4 @@
 var React = require('react');
-var { ListenerMixin } = require('reflux');
 
 var studentActions = require('../actions/studentActions.jsx');
 var studentStore = require('../stores/studentStore.jsx');
@@ -19,31 +18,32 @@ var {
 } = require('react-bootstrap');
 
 var StudentList = module.exports = React.createClass({
-	mixins: [ListenerMixin, Navigation],
 	getInitialState: function(){
-		return {
-			students: null,
-			query:'',
-      filteredData: this.props.data
-		};
+    return {
+      filtered:null
+    };
+  },
+  componentWillMount: function(){
+  	this.setState({
+  		filtered:this.props.students
+  	});
+  },
+	componentWillReceiveProps: function(newProps){
+		this.doSearch(newProps.query);
 	},
-	componentWillMount: function() {
-		this.listenTo(studentStore, this.studentsUpdate, function(initialStudents) {
-			this.setState({
-				students: initialStudents
-			});
-		});
-	},
-	studentsUpdate: function(latestStudents) {
-		this.setState({
-			students: latestStudents
-		});
-	},
+	doSearch:function(queryText){
+		var queryResult=[];
 
-
-	render: function() {
-		var content;
-		var students = this.state.students;
+		this.props.students.forEach(function(person){
+     	if((person.firstName.toLowerCase().indexOf(queryText)!=-1 )||(person.lastName.toLowerCase().indexOf(queryText)!=-1 ))
+       	queryResult.push(person);
+		});
+    this.setState({
+    	filtered:queryResult
+    });
+  },
+ 	render: function() {
+		var students = this.state.filtered;
 		var view;
 		if (!students) {
 			view = (
@@ -62,7 +62,7 @@ var StudentList = module.exports = React.createClass({
 				});
 				return (
 					<tr key={key++}>
-						<td onClick={StudentList.viewSingleStudent}>{student.firstName + " " + student.lastName}</td>
+						<td>{student.firstName + " " + student.lastName}</td>
 						<td>{student.phone}</td>
 						<td>{emails}</td>
 						<td>{student.rankId}</td>
--- server/views/components/students.jsx
+++ server/views/components/students.jsx
@@ -1,28 +1,48 @@
 var React = require('react');
 var Reflux = require('reflux');
+var { ListenerMixin } = Reflux;
 var {
 	RouteHandler,
-	Link
+	Link,
+  Navigation
 } = require('react-router');
 var {
 	Button,
 	Grid,
 	ButtonToolbar,
 	Row,
-	Col
+	Col,
+  Input
 } = require('react-bootstrap');
-var SearchBox = React.createClass({
-    doSearch:function(){
-        var query=this.refs.searchInput.getDOMNode().value; // this is the search text
-        this.props.doSearch(query);
-    },
-    render:function(){
-        return <input type="text" ref="searchInput" placeholder="Search Name" value={this.props.query} onChange={this.doSearch}/>
-    }
-});
+var studentActions = require('../actions/studentActions.jsx');
+var studentStore = require('../stores/studentStore.jsx');
+
 
 var Students = module.exports = React.createClass({
-	
+  mixins: [ListenerMixin, Navigation],
+  getInitialState: function(){
+    return {
+      students: null,
+      query:'',
+    };
+  },
+  componentWillMount: function() {
+    this.listenTo(studentStore, this.studentsUpdate, function(initialStudents) {
+      this.setState({
+        students: initialStudents
+      });
+    });
+  },
+  studentsUpdate: function(latestStudents) {
+    this.setState({
+      students: latestStudents
+    });
+  },
+	doSearch:function(){
+    this.setState({
+      query: this.refs.searchInput.getValue()
+    });
+  },
 	render: function(){
 		var addButton= (
 			<ButtonToolbar>
@@ -32,7 +52,7 @@ var Students = module.exports = React.createClass({
 		var toolbar =(
 			<Grid>
 				<Row className="show-grid">
-          			<Col xs={6} md={4}><SearchBox query={studentList.state.query} doSearch={this.doSearch}/></Col>
+          			<Col xs={6} md={4}><Input type="text" ref="searchInput" onChange={this.doSearch} placeholder="Search Name"/></Col>
           			<Col xs={6} md={4}><h4 className="text-center">STUDENT MANAGEMENT TOOLBAR</h4></Col>
           			<Col xs={6} md={4}><span className="pull-right">{addButton}</span></Col>
        			</Row>
@@ -41,7 +61,7 @@ var Students = module.exports = React.createClass({
 		return(
 			<div className="students">
 				{toolbar}
-				<RouteHandler routerParams={this.props.routerParams} />
+				<RouteHandler routerParams={this.props.routerParams} query={this.state.query} students={this.state.students} />
 			</div>
 		);
 	}
