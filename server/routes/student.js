f907d5aa2cfe4a520488f9d4aeb46a4b6865d985
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-04-01 14:19:12 -0400

    removed debugger statement

--- server/routes/student.js
+++ server/routes/student.js
@@ -115,7 +115,7 @@ module.exports = function() {
             - A status code of success/failure
             - If successful, the altered student data object passed in by the request
     */
-    .put(function(req, res, next) { debugger;
+    .put(function(req, res, next) {
       var validData = true;
       var studentProperties = [
         "_id", "firstName", "lastName", "gender",
