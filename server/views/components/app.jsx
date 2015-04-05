3a2fac1e2f4ab45c467aff82d4311ea435a969f5
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2015-04-05 12:42:20 -0400

    Fix app.jsx formatting

--- server/views/components/app.jsx
+++ server/views/components/app.jsx
@@ -1,145 +1,138 @@
 var React = require('react');
 var Router = require('react-router');
 var {
-    ListenerMixin
-    } = require('reflux');
+  ListenerMixin
+} = require('reflux');
 
 // EcmaScript6 destructuring assignment syntax.
 // Equivalent to:
 //   var RouteHandler = Router.RouteHandler;
 //   ...
 var {
-    RouteHandler
-    } = Router;
+  RouteHandler
+} = Router;
 
 var LoginUI = require('./login.jsx');
 var Banner = require('./banner.jsx');
 var {
-    logIn
-    } = require('../actions/authActions.jsx');
+  logIn
+} = require('../actions/authActions.jsx');
 
 var {
-    Grid,
-    Row,
-    Col,
+  Grid,
+  Row,
+  Col,
 
-    Navbar,
-    Nav
-    } = require('react-bootstrap');
+  Navbar,
+  Nav
+} = require('react-bootstrap');
 
 var {
-    NavItemLink
-    } = require('react-router-bootstrap');
+  NavItemLink
+} = require('react-router-bootstrap');
 
 // Note, each route is actually a name
 // corresponding with the React-Router
 // configuration in router.jsx
 var nav = {
-    "Students": "students",
-    "Ranks": "welcome2",
-    "Classes": "welcome3",
-    "Attendance": "welcome4"
+  "Students": "students",
+  "Ranks": "welcome2",
+  "Classes": "welcome3",
+  "Attendance": "welcome4"
 };
 
 // Unique key for each link
 var headerLinkId = 0;
 
 var App = React.createClass({
-    mixins: [ListenerMixin],
+  mixins: [ListenerMixin],
 
-    componentWillMount: function() {
-        var that = this;
+  componentWillMount: function() {
+    var that = this;
 
-        this.listenTo(logIn, function() {
-            that.setState({
-                loggedIn: true
-            });
-        });
-    },
+    this.listenTo(logIn, function() {
+      that.setState({
+        loggedIn: true
+      });
+    });
+  },
 
-    getInitialState: function() {
-        return {
-            token: localStorage["token"],
-            loggedIn: false
-        };
-    },
+  getInitialState: function() {
+    return {
+      token: localStorage["token"],
+      loggedIn: false
+    };
+  },
 
-    render: function() {
-        var linkText = Object.keys(nav);
-        var links = linkText.map(function(linkText) {
-            return ( < NavItemLink to = {
-                nav[linkText]
-            }
-            key = {
-                headerLinkId++
-            } >
-            {
-                linkText
-            } < /NavItemLink>
-            );
-        });
-        var view;
+  render: function() {
+    var linkText = Object.keys(nav);
+    var links = linkText.map(function(linkText) {
+      return (
+        <NavItemLink to={nav[linkText]} key={headerLinkId++}>
+          {linkText}
+        </NavItemLink>
+      );
+    });
+    var view;
 
-        if (!this.state.loggedIn) {
-            view = ( < div id = "main" >
-            < Navbar fixedTop = {
-                true
-            }
-            brand = {
-                "OpenDojo CMS"
-            }
-            /> < Banner / >
-            < Grid >
-            < LoginUI / >
-            { /* Footer */ } < Row >
-            < Navbar fixedBottom = {
-                true
-            }
-            brand = {
-                [< span >&copy;< /span>, " 2015 Team Nariyuki & Seneca College"]} / >
-            < /Row> < /Grid> < /div>
-        );
-        }
-        else {
-            view = ( < div id = "main" >
-            < Navbar fixedTop = {
-                true
-            }
-            brand = {
-                "OpenDojo CMS"
-            }
-            /> < Banner / >
-            < Grid >
-            { /* Main Content */ } < Row >
-            { /* Navbar */ } < Col md = {
-                3
-            } >
-            < div className = "sidebar-nav" >
-            < Navbar >
-            < Nav >
-            {
-                links
-            } < /Nav> < /Navbar> < /div> < /Col>
-            { /* Child View */ } < Col md = {
-                9
-            } >
-            < RouteHandler routerParams = {
-                this.props.routerParams
-        }
-        /> < /Col> < /Row>
-        { /* Footer */ } < Row >
-        < Navbar fixedBottom = {
-            true
-        }
-        brand = {
-            [< span >&copy;< /span>, " 2015 Team Nariyuki & Seneca College"]} / >
-        < /Row> < /Grid> < /div>
-        );
+    if (!this.state.loggedIn) {
+      view = (
+        <div id="main">
+          { /* Header */ }
+          <Navbar fixedTop={true} brand={"OpenDojo CMS"}/>
+
+          { /* Main banner */ }
+          <Banner />
+
+          <Grid>
+            <LoginUI />
+            { /* Footer */ }
+            <Row>
+              <Navbar fixedBottom={true} brand={[
+                <span>&copy;</span>, " 2015 Team Nariyuki & Seneca College"
+              ]} />
+            </Row>
+          </Grid>
+        </div>
+      );
+    }
+    else {
+      view = (
+        <div id = "main">
+          <Navbar fixedTop = {true} brand={"OpenDojo CMS"}/>
+          <Banner />
+
+          <Grid>
+            { /* Main Content */ }
+            <Row>
+              { /* Navbar */ }
+              <Col md={3}>
+                <div className="sidebar-nav">
+                  <Navbar>
+                    <Nav>
+                      {links}
+                    </Nav>
+                  </Navbar>
+                </div>
+              </Col>
+              { /* Child View */ }
+              <Col md = {9}>
+                <RouteHandler routerParams = {this.props.routerParams}/>
+              </Col>
+            </Row>
+            { /* Footer */ }
+            <Row>
+              <Navbar fixedBottom={true} brand={[
+                <span>&copy;</span>, " 2015 Team Nariyuki & Seneca College"
+              ]} />
+            </Row>
+          </Grid>
+        </div>
+      );
     }
 
     return view;
-}
-
+  }
 });
 
 module.exports = App;
