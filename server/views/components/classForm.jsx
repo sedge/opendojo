9a62b32bb5c51e157e0277b9cedd0d2ac90333ae
AlinaShtramwasser <nakhamchikalina@gmail.com>
2015-04-10 17:58:01 -0400

    fixed typo

--- server/views/components/classForm.jsx
+++ server/views/components/classForm.jsx
@@ -120,7 +120,7 @@ var ClassForm = module.exports = React.createClass({
             <option value="1">Monday</option>
             <option value="2">Tuesday</option>
             <option value="3">Wednesday</option>
-            <option value="4">Thurday</option>
+            <option value="4">Thursday</option>
             <option value="5">Friday</option>
             <option value="6">Saturday</option>
             <option value="7">Sunday</option>
